242c4854f854d1703003881a3130fc95c4b013d5
satte1 <smdigital469@gmail.com>
2026-04-28 14:59:47 +0530

    Updated database config for Railway

--- config/db_config.py
+++ config/db_config.py
@@ -1,10 +1,22 @@
+# import mysql.connector
+
+# def get_connection():
+#     connection = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="root",  # apna actual password yahan
+#         database="coaching_center"
+#     )
+#     return connection 
 import mysql.connector
+import os
 
 def get_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",  # apna actual password yahan
-        database="coaching_center"
+        host=os.environ.get('MYSQL_HOST', 'gondola.proxy.rlwy.net'),
+        port=int(os.environ.get('MYSQL_PORT', '39724')),
+        user=os.environ.get('MYSQL_USER', 'root'),
+        password=os.environ.get('MYSQL_PASSWORD', 'PhrsLoGXMFAIRxkCBBqGhcEpGvOQjiZG'),
+        database=os.environ.get('MYSQL_DATABASE', 'railway')
     )
     return connection
